cbaebf257f1b352eb4018b1ca9b3e8592ae684e9
Ivan Soldatov <yazvyazda@mail.ru>
2015-12-24 16:48:26 +0300

    add block goods-list

---
+++ common.blocks/goods-list/goods-list.bemtree.js
@@ -0,0 +1,171 @@
+block('goods-list').content()(function() {
+	
+	var items = ([
+		{
+			"goods-id": 1,
+			"title": "Ergobaby — Organic Navy",
+			"name": "Organic Navy",
+			"category": 1,
+			"preview": "/assets/i/promo1.jpg",
+			"image": [
+				"/assets/i/promo1.jpg",
+				"/assets/i/promo1-1.jpg"
+			],
+			"price": [
+				{
+					"old": 10000,
+					"current": 7500
+				}
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 2,
+			"title": "Ergobaby — Organic Bundle of Joy - Navy",
+			"name": "Organic Bundle of Joy - Navy",
+			"category": 1,
+			"preview": "/assets/i/promo2.jpg",
+			"image": [
+				"/assets/i/promo2.jpg",
+				"/assets/i/promo2-1.jpg",
+				"/assets/i/promo2-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 3,
+			"title": "Ergobaby — Original Bundle of Joy - Black/Camel",
+			"name": "Original Bundle of Joy - Black/Camel",
+			"category": 1,
+			"preview": "/assets/i/promo3.jpg",
+			"image": [
+				"/assets/i/promo3.jpg",
+				"/assets/i/promo3-1.jpg",
+				"/assets/i/promo3-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 4,
+			"title": "Ergobaby — Organic Zen",
+			"name": "Organic Zen",
+			"category": 1,
+			"preview": "/assets/i/promo4.jpg",
+			"image": [
+				"/assets/i/promo4.jpg",
+				"/assets/i/promo4-1.jpg",
+				"/assets/i/promo4-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 5,
+			"title": "Ergobaby — Organic Dandelion",
+			"name": "Organic Dandelion",
+			"category": 1,
+			"preview": "/assets/i/promo5.jpg",
+			"image": [
+				"/assets/i/promo5.jpg",
+				"/assets/i/promo5-1.jpg",
+				"/assets/i/promo5-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 6,
+			"title": "Ergobaby — Organic Quartz",
+			"name": "Organic Quartz",
+			"category": 1,
+			"preview": "/assets/i/promo6.jpg",
+			"image": [
+				"/assets/i/promo6.jpg",
+				"/assets/i/promo6-1.jpg",
+				"/assets/i/promo6-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 7,
+			"title": "Ergobaby — Organic Dark Cocoa",
+			"name": "Organic Dark Cocoa",
+			"category": 1,
+			"preview": "/assets/i/promo7.jpg",
+			"image": [
+				"/assets/i/promo7.jpg",
+				"/assets/i/promo7-1.jpg",
+				"/assets/i/promo7-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 8,
+			"title": "Ergobaby — Original Teal",
+			"name": "Original Teal",
+			"category": 1,
+			"preview": "/assets/i/promo8.jpg",
+			"image": [
+				"/assets/i/promo8.jpg",
+				"/assets/i/promo8-1.jpg",
+				"/assets/i/promo8-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 9,
+			"title": "Ergobaby — Original Red",
+			"name": "Original Red",
+			"category": 1,
+			"preview": "/assets/i/promo9.jpg",
+			"image": [
+				"/assets/i/promo9.jpg",
+				"/assets/i/promo9-1.jpg",
+				"/assets/i/promo9-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		},
+		{
+			"goods-id": 10,
+			"title": "Ergobaby — Original Indigo Mint Dots",
+			"name": "Original Indigo Mint Dots",
+			"category": 1,
+			"preview": "/assets/i/promo10.jpg",
+			"image": [
+				"/assets/i/promo10.jpg",
+				"/assets/i/promo10-1.jpg",
+				"/assets/i/promo10-2.jpg"
+			],
+			"price": [
+				{ "current": 10000 }
+			],
+			"desc": "Назначение — городской"
+		}
+	]);
+
+    return [
+    	
+    ];
+});
