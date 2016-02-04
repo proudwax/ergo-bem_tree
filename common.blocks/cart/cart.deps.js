f0ab2f1fcdbf6c385637bb47772109443ec159ff
Ivan Soldatov <yazvyazda@mail.ru>
2016-02-04 12:56:10 +0300

    add block cart tech:js rub bemhtml

--- common.blocks/cart/cart.deps.js
+++ common.blocks/cart/cart.deps.js
@@ -4,7 +4,10 @@
 	],
     shouldDeps: [
 		{ block: 'link', mods: { theme: 'ergo' } },
-		{ block: 'rub' },
 		{ block: 'amount' }
-	]
+	],
+	tech: 'js',
+    mustDeps: [
+        { block: 'rub', tech: 'bemhtml' }
+    ]
 })
--- common.blocks/cart/cart.js
+++ common.blocks/cart/cart.js
@@ -1,13 +1,42 @@
-modules.define('cart', ['i-bem__dom'], function(provide, BEMDOM) {
+modules.define('cart', ['i-bem__dom', 'BEMHTML'], function(provide, BEMDOM, BEMHTML) {
 
 provide(BEMDOM.decl(this.name, {
     onSetMod : {
         'js' : {
             'inited' : function() {
-                
+				this._items = this.findBlocksInside('cart-item');
+				
+                this.elem('total').html(this._getText());
+
             }
         }
-    }
+    },
+	
+	_getTotal: function(){
+		this._total = this._items.reduce(function(total, cur){
+			total.count = total.count + cur.params.count;		
+			total.sum = total.sum + cur.params.count * cur.params.price;
+
+			return {'count': total.count, 'sum': total.sum}
+		}, {'count': 0, 'sum': 0});
+	},
+	
+	_getEnding: function(count){
+		ending = ['ов', '', 'а', 'а', 'а', 'ов', 'ов', 'ов', 'ов', 'ов'];
+		
+		return ending[count % 10];
+	},
+	
+	_getText: function(){
+		this._getTotal();
+	
+		rub = [{
+			block: 'rub',
+			mods: { size: 'small' }
+		}];
+	
+		return 'Итого: ' + this._total.count + ' товар' + this._getEnding(this._total.count) + ' на сумму <strong>' + this._total.sum + BEMHTML.apply(rub) + '</strong>';
+	}
 }));
 
 });
