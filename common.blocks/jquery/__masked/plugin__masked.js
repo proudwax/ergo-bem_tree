36d02342426a5b26c67ff74e701afc1c4c4c5477
Ivan <yazvyazda@mail.ru>
2016-02-24 11:42:20 +0300

    del files

--- common.blocks/jquery/__masked/plugin__masked.js
+++
@@ -1,8 +0,0 @@
-modules.define('plagin__masked', ['jquery'], function(provide, $) {
-
- /* //borschik:include:../../libs/jquery-mask-plugin/dist/jquery.mask.js */
- /* //borschik:include:../../libs/jquery.maskedinput/dist/jquery.maskedinput.js */
- 
-provide($);
-
-});
--- common.blocks/sticky/_cart-button/sticky_cart-button.deps.js
+++
@@ -1,5 +0,0 @@
-({
-    shouldDeps: [
-        { block: 'cart-button' }
-    ]
-})
