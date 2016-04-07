2e0554094149b793849163e6e40e951fb419bc9f
Ivan <yazvyazda@mail.ru>
2016-04-07 08:24:12 +0300

    sidebar translateX

--- common.blocks/sidebar/sidebar.js
+++ common.blocks/sidebar/sidebar.js
@@ -4,14 +4,35 @@ provide(BEMDOM.decl(this.name, {
     onSetMod : {
         'js' : {
             'inited' : function() {
-				var _this = this;
-					/* screenWidth = win.; */
+				var _this = this,
+					showFlag = 0;
+
+				this._pageContainer = this.findBlockOutside('page').elem('container');
 				
-				console.log(win);
+				// Ширина окна - (ширина контента + (ширина sidebar + отступ) * 2) 
+				// >= 0 - показывать sidebar
+				// < 0 - открывать по модификатору show
+				showFlag = (BEMDOM.win.width() - this._pageContainer.width()) / 2;
+
+				console.log(showFlag);
 				
-				/* this.domElem.css() */
+					// this.domElem.css({'transform': 'translateX(-200)'});
+
+				if(showFlag >= this.domElem.width()){
+					this.domElem.css({
+						'transform': 'translateX(-' + (this.domElem.width() + 16) + 'px)'
+					});
+				}else{
+					this.domElem.css({
+						'transform': 'translateX(-' + (this.domElem.width() + 16 + Math.abs(showFlag) ) + 'px)'
+					});
+				}
 			}
         }
+    },
+
+    _resize: function(){
+
     }	
 
 }));
