645d53a6d95975cf1ef9360321fb4e3f6fad184b
Ivan <yazvyazda@mail.ru>
2016-04-12 23:10:37 +0300

    add form block

---
+++ common.blocks/form/form.deps.js
@@ -0,0 +1,8 @@
+({
+    mustDeps : [
+        { block : 'i-bem', elems : ['dom'] }
+    ],
+	shouldDeps: [
+        { block: 'button' }
+    ]
+})
--- common.blocks/sidebar/sidebar.deps.js
+++ common.blocks/sidebar/sidebar.deps.js
@@ -8,6 +8,7 @@
 		
 		// Дополнительные, что в контенте .bemtree
         { block: 'checkbox-group', mods: { theme: 'islands', size : 'l' } },
-        { block: 'radio-group', mods: { theme: 'islands', size : 'l' } }
+        { block: 'radio-group', mods: { theme: 'islands', size : 'l' } },
+        { block: 'form' }
     ]
 })
